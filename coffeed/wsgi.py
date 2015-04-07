0bfef26a1a8089ecaf4bc5c0aea1b9b36ca1df21
Matthew Rossi <matthewrossi@Matthew-Rossis-MacBook-Pro-9.local>
2015-04-07 14:55:15 -0400

    heroku push 3

--- coffeed/wsgi.py
+++ coffeed/wsgi.py
@@ -6,11 +6,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
-
 import os
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coffeed.settings")
 
 from django.core.wsgi import get_wsgi_application
+
 from dj_static import Cling
 
 application = Cling(get_wsgi_application())
+
